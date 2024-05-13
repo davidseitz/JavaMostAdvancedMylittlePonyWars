96ff39d7d606f51ce3ab44b87d643be7fde90e80
davidseitz <david.seitz@ba-racing-team.de>
2024-05-13 15:42:13 +0200

    Was left out it seems

--- src/Logic/Groundfigures.java
+++ src/Logic/Groundfigures.java
@@ -1,5 +1,20 @@
 package Logic;
 
 public class Groundfigures extends Figure {
-
+	private int x;
+	private int y;
+	public Groundfigures(int x, int y) {
+		this.x = x;
+		this.y = y;
+	}
+	
+	@Override
+	/**
+	 * This method is used to move the ground figures
+	 * @return boolean true if the move is successful, false otherwise
+	 */
+	public boolean move(int x, int y, int new_x, int new_y) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 }
